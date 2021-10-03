e29681276c031413df1c2cca878056fd55753cf6
SabbirHossain07 <sabbirsopnil07@gmail.com>
2021-10-03 21:35:28 +0600

    Initial Commit

---
+++ Carsousel/CarsouselApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarsouselApp.swift
+//  Carsousel
+//
+//  Created by Sopnil Sohan on 3/10/21.
+//
+
+import SwiftUI
+
+@main
+struct CarsouselApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
